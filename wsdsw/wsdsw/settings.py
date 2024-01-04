48e09a7566f8b9b080f3ed3f91b09bfb5b061576
Tarnov <82037878+Tarnov@users.noreply.github.com>
2024-01-04 18:04:37 +0300

    DEV 2 art

--- wsdsw/wsdsw/settings.py
+++ wsdsw/wsdsw/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0s=l8ysh#fdx(-8!cu%cp5uy$7s%k$t73um^o%j3)7w*gnv6@y'
+SECRET_KEY = 'go fuck you self'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
